443006b8e04b7577ef318ed1aef43f1f89ee2a35
minhlnd0302 <minhlnd.hcmue@gmail.com>
2020-10-10 08:43:36 +0700

    db querry login

--- OrderCoffee/OrderCoffee/Controllers/HomeController.cs
+++ OrderCoffee/OrderCoffee/Controllers/HomeController.cs
@@ -1,13 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Dapper;
+using OrderCoffee.Models;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace OrderCoffee.Controllers
 {
     public class HomeController : Controller
     {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ToString());
+        public SqlConnection Conn
+        {
+            get
+            {
+                if (conn == null)
+                {
+                    conn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ToString());
+                }
+                return conn;
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +40,34 @@ namespace OrderCoffee.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string id_login_email, string id_login_password)
+        {
+
+            string queryFindEmail = "Select * From dbo.Account Where  Email = '" + id_login_email + "'";
+            string queryFindUserName = "Select * From dbo.Account Where UserName = '" + id_login_email + "'";
+
+            var user = new Account();
+            using (IDbConnection db = conn)
+            {
+                user = db.Query<Account>(queryFindUserName).FirstOrDefault();
+
+                if (user == null) {
+                    user = db.Query<Account>(queryFindEmail).FirstOrDefault();
+                }
+            }
+
+            if (user != null)
+            {
+                if(user.PassWord == id_login_password)
+                {
+                    return View("AdminDashboard");
+                }
+            }
+            var tmp = id_login_email;
+            return View("Index");
         }
     }
 }
---
+++ OrderCoffee/OrderCoffee/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderCoffee.Models
+{
+    public class Account
+    {
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string PassWord { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int Roles { get; set; }
+    }
+}
