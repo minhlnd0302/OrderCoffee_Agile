67b06242c82ab40ebaa447cf2fb67067a4ece162
minhlnd0302 <minhlnd.hcmue@gmail.com>
2020-10-11 21:51:40 +0700

    login

---
+++ OrderCoffee/DBManager/DBController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DatabaseProject
+{
+    public class DBController
+    {
+        private static DBController instance = null;
+
+        public static DBController Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DBController();
+                }
+                return instance;
+            }
+        }
+
+
+        //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ToString());
+
+        private DBController()
+        {
+
+        }
+    }
+}
