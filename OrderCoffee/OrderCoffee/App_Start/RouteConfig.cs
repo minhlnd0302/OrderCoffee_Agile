8c542d6aa20c8666f5307fbe624bded40e701035
minhlnd0302 <minhlnd.hcmue@gmail.com>
2020-11-03 21:45:29 +0700

    add template

--- OrderCoffee/OrderCoffee/App_Start/RouteConfig.cs
+++ OrderCoffee/OrderCoffee/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OrderCoffee
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Products", action = "Products", id = UrlParameter.Optional }
             );
         }
     }
