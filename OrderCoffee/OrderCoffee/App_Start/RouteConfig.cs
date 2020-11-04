c7c8a7709448074e4e615b846f723f902e255595
minhlnd0302 <minhlnd.hcmue@gmail.com>
2020-11-04 14:50:58 +0700

    test login

--- OrderCoffee/OrderCoffee/App_Start/RouteConfig.cs
+++ OrderCoffee/OrderCoffee/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OrderCoffee
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Products", action = "Products", id = UrlParameter.Optional }
+                defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- OrderCoffee/OrderCoffee/Controllers/ProductsController.cs
+++ OrderCoffee/OrderCoffee/Controllers/ProductsController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using OrderCoffee.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Dapper;
 
 namespace OrderCoffee.Controllers
 {
     public class ProductsController : Controller
     {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ToString());
         // GET: Products
         public ActionResult Index()
         {
+            string querySelectProduct = "Select * from product";
+            using (IDbConnection db = conn)
+            {
+                var listProduct = new List<Product>(); 
+                listProduct = db.Query<Product>(querySelectProduct).ToList();  
+                ViewBag.listProduct = listProduct;
+            }
             return View();
         }
 
