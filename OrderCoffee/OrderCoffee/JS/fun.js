521dd526c38b20bc37b607069c62ac670c2b612c
minhlnd0302 <minhlnd.hcmue@gmail.com>
2020-11-05 14:18:14 +0700

    OC-9 : add to cart from list item

---
+++ OrderCoffee/OrderCoffee/JS/fun.js
@@ -0,0 +1,36 @@
+﻿function addProductToListCart(item) {
+    debugger
+    var cartList = new Array();
+    try { 
+        cartList = JSON.parse(localStorage.getItem('cartList'));
+        if (cartList == null) {
+            cartList = new Array();
+        }
+    }
+    catch{
+        localStorage.setItem('cartList', JSON.stringify([]));
+    }
+    
+    console.log(cartList);
+    console.log(typeof cartList)
+
+    // get product current
+    {
+        var listProduct = JSON.parse(sessionStorage.getItem('listProduct'));
+        var idProduct = item.id; 
+
+        var tmp = listProduct.filter(item =>
+            item.Id == idProduct
+        )
+    } 
+    cartList.push(tmp[0])
+    console.log(cartList)
+
+
+    localStorage.setItem('cartList', JSON.stringify(cartList));
+
+    debugger
+    window.location = "/Products/Index";
+
+    console.log(idProduct)
+}
