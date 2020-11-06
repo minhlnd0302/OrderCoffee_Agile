0cd261dc3e0137ee7a5a130a10db9e4c53ba34ac
minhlnd0302 <minhlnd.hcmue@gmail.com>
2020-11-06 12:37:03 +0700

    OC-10: xem chi tiet

--- OrderCoffee/OrderCoffee/JS/fun.js
+++ OrderCoffee/OrderCoffee/JS/fun.js
@@ -21,9 +21,23 @@
 
         var tmp = listProduct.filter(item =>
             item.Id == idProduct
-        )
+        ) 
     } 
-    cartList.push(tmp[0])
+
+    var tonTai = 0; 
+    for (var index in cartList) {
+        if (cartList[index].Id == idProduct) {
+            cartList[index].SoLuong += 1;
+            tonTai = 1;
+        }
+    }
+
+    if (tonTai == 0) {
+        tmp[0].SoLuong = 1;
+        cartList.push(tmp[0])
+    }
+    
+
     console.log(cartList)
 
 
@@ -34,3 +48,80 @@
 
     console.log(idProduct)
 }
+
+function setWatchingProductId(item) {
+    var tmp = item.id;
+    sessionStorage.setItem('id', tmp);
+    debugger
+    tmp = parseInt(tmp[5]) * 100 + parseInt(tmp[6]) * 10 + parseInt(tmp[7]);  
+    sessionStorage.setItem('currentProductId', tmp - 1) 
+
+    setInfoCurrentProductModal();
+}
+
+function setInfoCurrentProductModal() {
+    var index = parseInt(sessionStorage.getItem('currentProductId'));
+    var list = JSON.parse(sessionStorage.getItem('listProduct'));
+    var pro = list[index];
+
+    $('#img1').attr("src", "../" + pro.Image);
+    $('#img2').attr("src", "../" + pro.Image);
+    $('#img3').attr("src", "../" + pro.Image);
+    $('#img4').attr("src", "../" + pro.Image);
+    $('#img5').attr("src", "../" + pro.Image);
+    $('#img6').attr("src", "../" + pro.Image);
+    $('#img7').attr("src", "../" + pro.Image);
+    $('#img8').attr("src", "../" + pro.Image);
+
+    $('#productName').text(pro.Name);
+    $('#price').text(pro.Price); 
+    return pro;
+}
+
+function addToCart() {
+
+    var idProduct = sessionStorage.getItem('id')
+    var sl = $('#slMua').val();
+    var cartList = new Array();
+    try {
+        cartList = JSON.parse(localStorage.getItem('cartList'));
+        if (cartList == null) {
+            cartList = new Array();
+        }
+    }
+    catch{
+        localStorage.setItem('cartList', JSON.stringify([]));
+    } 
+
+    // get product current
+    {
+        var listProduct = JSON.parse(sessionStorage.getItem('listProduct')); 
+
+        var tmp = listProduct.filter(item =>
+            item.Id == idProduct
+        )
+    }
+
+    var tonTai = 0;
+    for (var index in cartList) {
+        if (cartList[index].Id == idProduct) {
+            cartList[index].SoLuong += sl;
+            tonTai = 1;
+        }
+    }
+
+    if (tonTai == 0) {
+        tmp[0].SoLuong = sl; 
+        cartList.push(tmp[0])
+    }
+     
+    console.log(cartList)
+     
+    localStorage.setItem('cartList', JSON.stringify(cartList));
+
+    debugger
+    window.location = "/Products/Index";
+
+    console.log(idProduct)
+}
+     
